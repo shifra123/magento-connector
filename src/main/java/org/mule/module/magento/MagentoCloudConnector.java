c514142e97c27180122f491e4f8ef2d65ac639b9
Jose Sahad <jose.amenta@mulesoft.com>
2013-02-20 12:26:41 -0300

    Rollback to DevKit 3.3.2

--- src/main/java/org/mule/module/magento/MagentoCloudConnector.java
+++ src/main/java/org/mule/module/magento/MagentoCloudConnector.java
@@ -9,7 +9,6 @@
 package org.mule.module.magento;
 
 import org.mule.api.annotations.Configurable;
-import org.mule.api.annotations.ConnectivityTesting;
 import org.mule.api.annotations.Module;
 import org.mule.api.annotations.Processor;
 import org.mule.api.annotations.display.Password;
@@ -18,10 +17,6 @@ import org.mule.api.annotations.lifecycle.Start;
 import org.mule.api.annotations.param.Default;
 import org.mule.api.annotations.param.Optional;
 import org.mule.api.annotations.param.Payload;
-import org.mule.common.DefaultTestResult;
-import org.mule.common.Result;
-import org.mule.common.TestResult;
-import org.mule.common.Testable;
 import org.mule.module.magento.api.AxisPortProvider;
 import org.mule.module.magento.api.DefaultAxisPortProvider;
 import org.mule.module.magento.api.MagentoClientAdaptor;
@@ -56,8 +51,8 @@ import java.util.Map;
  *
  * @author MuleSoft, Inc.
  */
-@Module(name = "magento", schemaVersion = "1.1", friendlyName = "Magento", connectivityTesting = ConnectivityTesting.ON)
-public class MagentoCloudConnector implements Testable {
+@Module(name = "magento", schemaVersion = "1.1", friendlyName = "Magento")
+public class MagentoCloudConnector {
 
     private MagentoOrderClient<Map<String, Object>, List<Map<String, Object>>, MagentoException> orderClient;
     private MagentoCustomerClient<Map<String, Object>, List<Map<String, Object>>, MagentoException> customerClient;
@@ -1790,22 +1785,6 @@ public class MagentoCloudConnector implements Testable {
 
     public void setAddress(String address) {
         this.address = address;
-    }
-
-    /**
-     * Method used by Mule Studio's connectivy testing
-     * @return testing results
-     */
-    @Override
-    public TestResult test() {
-        try {
-            initialiseConnector();
-            getCatalogCurrentStoreView();
-        }
-        catch (Exception e) {
-            return new DefaultTestResult(TestResult.Status.FAILURE, "Bad credentials");
-        }
-        return new DefaultTestResult(TestResult.Status.SUCCESS);
     }
 
     private class PortProviderInitializer {
