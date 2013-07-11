cd55686eced98f8fd1282a717f66e2750f387cd0
jsaliba <justin.saliba@ricston.com>
2013-07-11 16:27:04 +0200

    Added get customer address test cases

---
+++ src/test/java/org/mule/module/magento/automation/testcases/GetCustomerAddressTestCases.java
@@ -0,0 +1,77 @@
+package org.mule.module.magento.automation.testcases;
+
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
+
+import java.util.HashMap;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.experimental.categories.Category;
+import org.mule.api.MuleEvent;
+import org.mule.api.processor.MessageProcessor;
+
+import com.magento.api.CustomerAddressEntityCreate;
+import com.magento.api.CustomerAddressEntityItem;
+import com.magento.api.CustomerCustomerEntityToCreate;
+
+public class GetCustomerAddressTestCases extends MagentoTestParent {
+
+	@Before
+	public void setUp() {
+		try {
+			testObjects = (HashMap<String, Object>) context.getBean("getCustomerAddress");
+			
+			CustomerCustomerEntityToCreate customer = (CustomerCustomerEntityToCreate) testObjects.get("customerRef");
+			int customerId = createCustomer(customer);
+			
+			testObjects.put("customerId", customerId);
+			
+			CustomerAddressEntityCreate address = (CustomerAddressEntityCreate) testObjects.get("customerAddressRef");
+			int addressId = createCustomerAddress(customerId, address);
+		
+			testObjects.put("addressId", addressId);
+		}
+		catch (Exception e) {
+			e.printStackTrace();
+			fail();
+		}
+	}
+	
+	@Category({ RegressionTests.class})
+	@Test
+	public void testGetCustomerAddress() {
+		try {
+			
+			int addressId = (Integer) testObjects.get("addressId");
+			
+			MessageProcessor flow = lookupFlowConstruct("get-customer-address");
+			MuleEvent response = flow.process(getTestEvent(testObjects));
+
+			CustomerAddressEntityItem address = (CustomerAddressEntityItem) response.getMessage().getPayload();			
+			assertNotNull(address);
+			assertTrue(addressId == address.getCustomer_address_id());
+		}
+		catch (Exception e) {
+			e.printStackTrace();
+			fail();
+		}
+	}
+	
+	@After
+	public void tearDown() {
+		try {
+			int customerAddressId = (Integer) testObjects.get("addressId");
+			deleteCustomerAddress(customerAddressId);
+			
+			int customerId = (Integer) testObjects.get("customerId");
+			deleteCustomer(customerId);
+		}
+		catch (Exception e) {
+			e.printStackTrace();
+			fail();
+		}
+	}
+}
