01f94f0db8ea29fabe6025ce25b330a7badc3fa2
jsaliba <justin.saliba@ricston.com>
2013-07-11 16:18:34 +0200

    Added delete customer address test cases

---
+++ src/test/java/org/mule/module/magento/automation/testcases/DeleteCustomerAddressTestCases.java
@@ -0,0 +1,68 @@
+package org.mule.module.magento.automation.testcases;
+
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
+
+import java.util.HashMap;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.experimental.categories.Category;
+import org.mule.api.MuleEvent;
+import org.mule.api.processor.MessageProcessor;
+
+import com.magento.api.CustomerAddressEntityCreate;
+import com.magento.api.CustomerCustomerEntityToCreate;
+
+public class DeleteCustomerAddressTestCases extends MagentoTestParent {
+
+	@Before
+	public void setUp() {
+		try {
+			testObjects = (HashMap<String, Object>) context.getBean("deleteCustomerAddress");
+			
+			CustomerCustomerEntityToCreate customer = (CustomerCustomerEntityToCreate) testObjects.get("customerRef");
+			int customerId = createCustomer(customer);
+			
+			testObjects.put("customerId", customerId);
+			
+			CustomerAddressEntityCreate address = (CustomerAddressEntityCreate) testObjects.get("customerAddressRef");
+			int addressId = createCustomerAddress(customerId, address);
+			
+			testObjects.put("addressId", addressId);
+		}
+		catch (Exception e) {
+			e.printStackTrace();
+			fail();
+		}
+	}
+	
+	@Category({SmokeTests.class, RegressionTests.class})
+	@Test
+	public void testDeleteCustomerAddress() {
+		try {
+			MessageProcessor flow = lookupFlowConstruct("delete-customer-address");
+			MuleEvent response = flow.process(getTestEvent(testObjects));
+			boolean result = (Boolean) response.getMessage().getPayload();
+			assertTrue(result);
+		}
+		catch (Exception e) {
+			e.printStackTrace();
+			fail();
+		}
+	}
+	
+	@After
+	public void tearDown() {
+		try {
+			int customerId = (Integer) testObjects.get("customerId");
+			deleteCustomer(customerId);
+		}
+		catch (Exception e) {
+			e.printStackTrace();
+			fail();
+		}
+	}
+	
+}
