75f6e424024917f204d092b6b53e140b7bdbcc7b
jsaliba <justin.saliba@ricston.com>
2013-07-11 09:13:16 +0200

    Added test parent and template resource files

---
+++ src/test/java/org/mule/magento/automation/testcases/MagentoTestParent.java
@@ -0,0 +1,38 @@
+package org.mule.magento.automation.testcases;
+
+import java.util.Map;
+
+import org.junit.BeforeClass;
+import org.junit.Rule;
+import org.junit.rules.Timeout;
+import org.mule.api.processor.MessageProcessor;
+import org.mule.tck.junit4.FunctionalTestCase;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class MagentoTestParent extends FunctionalTestCase {
+
+	// Set global timeout of tests to 10minutes
+    @Rule
+    public Timeout globalTimeout = new Timeout(600000);
+
+	protected static final String[] SPRING_CONFIG_FILES = new String[] { "AutomationSpringBeans.xml" };
+	protected static ApplicationContext context;
+	protected Map<String, Object> testObjects;
+	
+	@Override
+	protected String getConfigResources() {
+		return "automation-test-flows.xml";
+	}
+
+	protected MessageProcessor lookupFlowConstruct(String name) {
+		return (MessageProcessor) muleContext.getRegistry().lookupFlowConstruct(name);
+	}
+	
+	@BeforeClass
+	public static void beforeClass() {
+		context = new ClassPathXmlApplicationContext(SPRING_CONFIG_FILES);
+	}
+	
+	
+}
