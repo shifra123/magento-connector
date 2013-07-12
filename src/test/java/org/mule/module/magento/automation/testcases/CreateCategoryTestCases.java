b3a10f786dd4778de27010d60647cb5892fcb910
jsaliba <justin.saliba@ricston.com>
2013-07-12 10:36:40 +0200

    Added create, delete category test cases and helper methods

---
+++ src/test/java/org/mule/module/magento/automation/testcases/CreateCategoryTestCases.java
@@ -0,0 +1,58 @@
+package org.mule.module.magento.automation.testcases;
+
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
+
+import java.util.HashMap;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.experimental.categories.Category;
+import org.mule.api.MuleEvent;
+import org.mule.api.processor.MessageProcessor;
+
+public class CreateCategoryTestCases extends MagentoTestParent {
+
+	@Before
+	public void setUp() {
+		try {
+			testObjects = (HashMap<String, Object>) context.getBean("createCategory");
+		}
+		catch (Exception e) {
+			e.printStackTrace();
+			fail();
+		}
+	}
+	
+	@Category({SmokeTests.class, RegressionTests.class})
+	@Test
+	public void testCreateCategory() {
+		try {
+			MessageProcessor flow = lookupFlowConstruct("create-category");
+			MuleEvent response = flow.process(getTestEvent(testObjects));
+			
+			Integer categoryId = (Integer) response.getMessage().getPayload();
+			assertTrue(categoryId != null);
+			
+			testObjects.put("categoryId", categoryId);
+		}
+		catch (Exception e) {
+			e.printStackTrace();
+			fail();
+		}
+	}
+	
+	@After
+	public void tearDown() {
+		try {
+			int categoryId = (Integer) testObjects.get("categoryId");
+			deleteCategory(categoryId);
+		}
+		catch (Exception e) {
+			e.printStackTrace();
+			fail();
+		}
+	}
+	
+}
--- src/test/java/org/mule/module/magento/automation/testcases/MagentoTestParent.java
+++ src/test/java/org/mule/module/magento/automation/testcases/MagentoTestParent.java
@@ -12,6 +12,7 @@ import org.mule.tck.junit4.FunctionalTestCase;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
+import com.magento.api.CatalogCategoryEntityCreate;
 import com.magento.api.CustomerAddressEntityCreate;
 import com.magento.api.CustomerCustomerEntityToCreate;
 
@@ -68,6 +69,21 @@ public class MagentoTestParent extends FunctionalTestCase {
 	public boolean deleteCustomerAddress(int customerAddressId) throws Exception {
 		testObjects.put("addressId", customerAddressId);
 		MessageProcessor flow = lookupFlowConstruct("delete-customer-address");
+		MuleEvent response = flow.process(getTestEvent(testObjects));
+		return (Boolean) response.getMessage().getPayload();
+	}
+
+	public int createCategory(int parentId, CatalogCategoryEntityCreate category) throws Exception {
+		testObjects.put("parentId", parentId);
+		testObjects.put("catalogCategoryEntityRef", category);
+		MessageProcessor flow = lookupFlowConstruct("create-category");
+		MuleEvent response = flow.process(getTestEvent(testObjects));
+		return (Integer) response.getMessage().getPayload();
+	}
+	
+	public boolean deleteCategory(int categoryId) throws Exception {
+		testObjects.put("categoryId", categoryId);
+		MessageProcessor flow = lookupFlowConstruct("delete-category");
 		MuleEvent response = flow.process(getTestEvent(testObjects));
 		return (Boolean) response.getMessage().getPayload();
 	}
