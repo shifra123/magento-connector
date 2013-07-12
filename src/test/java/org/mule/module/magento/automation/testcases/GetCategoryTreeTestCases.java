e0fa3035bfd643c95910b42bceaa931c3e7e0111
jsaliba <justin.saliba@ricston.com>
2013-07-12 12:22:03 +0200

    Added get category tree test cases

---
+++ src/test/java/org/mule/module/magento/automation/testcases/GetCategoryTreeTestCases.java
@@ -0,0 +1,79 @@
+package org.mule.module.magento.automation.testcases;
+
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
+
+import java.util.HashMap;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.experimental.categories.Category;
+import org.mule.api.MuleEvent;
+import org.mule.api.processor.MessageProcessor;
+
+import com.magento.api.CatalogCategoryEntity;
+import com.magento.api.CatalogCategoryEntityCreate;
+import com.magento.api.CatalogCategoryInfo;
+import com.magento.api.CatalogCategoryTree;
+
+public class GetCategoryTreeTestCases extends MagentoTestParent {
+	@Before
+	public void setUp() {
+		try {
+			testObjects = (HashMap<String, Object>) context.getBean("getCategoryTree");
+			
+			CatalogCategoryEntityCreate parentCategory = (CatalogCategoryEntityCreate) testObjects.get("parentCategory");
+			CatalogCategoryEntityCreate childCategory = (CatalogCategoryEntityCreate) testObjects.get("childCategory");
+			
+			int parentId = (Integer) testObjects.get("parentId");
+			int parentCategoryId = createCategory(parentId, parentCategory);
+			int childCategoryId = createCategory(parentCategoryId, childCategory);
+			
+			testObjects.put("parentCategoryId", parentCategoryId);
+			testObjects.put("childCategoryId", childCategoryId);
+		}
+		catch (Exception e) {
+			e.printStackTrace();
+			fail();
+		}
+	}
+	
+	@Category({RegressionTests.class})
+	@Test
+	public void testGetCategoryTree() {
+		try {
+			int parentCategoryId = (Integer) testObjects.get("parentCategoryId");
+			int childCategoryId = (Integer) testObjects.get("childCategoryId");
+			
+			MessageProcessor flow = lookupFlowConstruct("get-category-tree");
+			MuleEvent response = flow.process(getTestEvent(testObjects));
+			
+			CatalogCategoryTree tree = (CatalogCategoryTree) response.getMessage().getPayload();
+			assertTrue(tree.getCategory_id() == parentCategoryId);
+			
+			CatalogCategoryEntity[] children = tree.getChildren();
+			CatalogCategoryEntity child = children[0]; // We only created 1 child.
+			
+			assertTrue(child.getCategory_id() == childCategoryId);
+			assertTrue(child.getParent_id() == parentCategoryId);
+		}
+		catch (Exception e) {
+			e.printStackTrace();
+			fail();
+		}
+	}
+	
+	@After
+	public void tearDown() {
+		try {
+			// Deleting the parent also deletes the child			
+			int parentCategoryId = (Integer) testObjects.get("parentCategoryId");
+			deleteCategory(parentCategoryId);
+		}
+		catch (Exception e) {
+			e.printStackTrace();
+			fail();
+		}
+	}
+}
