fe9a15d5a96b1b991df4d09c96d03766dea400d5
Justin Calleja <calleja.justin@gmail.com>
2013-07-12 13:15:10 +0200

    Added CreateProductTestCases.

--- src/test/java/org/mule/module/magento/automation/testcases/MagentoTestParent.java
+++ src/test/java/org/mule/module/magento/automation/testcases/MagentoTestParent.java
@@ -72,4 +72,10 @@ public class MagentoTestParent extends FunctionalTestCase {
 		return (Boolean) response.getMessage().getPayload();
 	}
 	
+	public int deleteProduct(int productId) throws Exception {
+		testObjects.put("productId", productId);
+		MessageProcessor flow = lookupFlowConstruct("delete-product");
+		MuleEvent response = flow.process(getTestEvent(testObjects));
+		return (Integer) response.getMessage().getPayload();
+	}
 }
