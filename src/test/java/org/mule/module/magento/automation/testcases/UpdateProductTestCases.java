8e9b2e6269989ec510ef961466bfee8860553ee0
Justin Calleja <calleja.justin@gmail.com>
2013-07-15 17:38:17 +0200

    Added UpdateProductTestCases.

---
+++ src/test/java/org/mule/module/magento/automation/testcases/UpdateProductTestCases.java
@@ -0,0 +1,60 @@
+package org.mule.module.magento.automation.testcases;
+
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
+
+import java.util.HashMap;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.experimental.categories.Category;
+import org.mule.api.MuleEvent;
+import org.mule.api.processor.MessageProcessor;
+
+public class UpdateProductTestCases extends MagentoTestParent {
+
+	@SuppressWarnings("unchecked")
+	@Before
+	public void setUp() {
+		try {
+			testObjects = (HashMap<String, Object>) context.getBean("updateProduct");
+			MessageProcessor flow = lookupFlowConstruct("create-product");
+			MuleEvent res = flow.process(getTestEvent(testObjects));
+			Integer productId = (Integer) res.getMessage().getPayload();
+			testObjects.put("productId", productId);
+		}
+		catch (Exception e) {
+			e.printStackTrace();
+			fail();
+		}
+	}
+	
+	@Category({SmokeTests.class, RegressionTests.class})
+	@Test
+	public void testUpdateProduct() {
+		try {
+			MessageProcessor flow = lookupFlowConstruct("update-product");
+			MuleEvent response = flow.process(getTestEvent(testObjects));
+			
+			assertTrue("Assert that the update was successful", (Boolean) response.getMessage().getPayload());
+		}
+		catch (Exception e) {
+			e.printStackTrace();
+			fail();
+		}
+	}
+	
+	@After
+	public void tearDown() {
+		try {
+			int productId = (Integer) testObjects.get("productId");
+			deleteProductById(productId);
+		}
+		catch (Exception e) {
+			e.printStackTrace();
+			fail();
+		}
+	}
+	
+}
