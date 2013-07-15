ffb9402ec17a58c0f5eab2e8805a6820b4efcb1e
jsaliba <justin.saliba@ricston.com>
2013-07-15 11:30:17 +0200

    Removed unnecessary initialisation

--- src/test/java/org/mule/module/magento/automation/testcases/GetCatalogCurrentStoreViewTestCases.java
+++ src/test/java/org/mule/module/magento/automation/testcases/GetCatalogCurrentStoreViewTestCases.java
@@ -3,10 +3,6 @@ package org.mule.module.magento.automation.testcases;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.fail;
 
-import java.util.HashMap;
-
-import org.junit.After;
-import org.junit.Before;
 import org.junit.Test;
 import org.junit.experimental.categories.Category;
 import org.mule.api.MuleEvent;
@@ -14,23 +10,12 @@ import org.mule.api.processor.MessageProcessor;
 
 public class GetCatalogCurrentStoreViewTestCases extends MagentoTestParent {
 
-	@Before
-	public void setUp() {
-		try {
-			testObjects = new HashMap<String, Object>();
-		}
-		catch (Exception e) {
-			e.printStackTrace();
-			fail();
-		}
-	}
-	
 	@Category({RegressionTests.class})
 	@Test
 	public void testGetCatalogCurrentStoreView() {
 		try {
 			MessageProcessor flow = lookupFlowConstruct("get-catalog-current-store-view");
-			MuleEvent response = flow.process(getTestEvent(testObjects));
+			MuleEvent response = flow.process(getTestEvent(null));
 			
 			Integer defaultStoreId = (Integer) response.getMessage().getPayload();
 			assertNotNull(defaultStoreId);
