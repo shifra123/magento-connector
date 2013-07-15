520c2192fa6e860bef5a9242cc4f787b5aa8494b
jsaliba <justin.saliba@ricston.com>
2013-07-15 16:01:35 +0200

    Added set shopping cart customer test cases

---
+++ src/test/java/org/mule/module/magento/automation/testcases/SetShoppingCartCustomerTestCases.java
@@ -0,0 +1,58 @@
+package org.mule.module.magento.automation.testcases;
+
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
+
+import java.util.HashMap;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.experimental.categories.Category;
+import org.mule.api.MuleEvent;
+import org.mule.api.processor.MessageProcessor;
+
+public class SetShoppingCartCustomerTestCases extends MagentoTestParent {
+
+	@Before
+	public void setUp() {
+		try {
+			testObjects = (HashMap<String, Object>) context.getBean("setShoppingCartCustomer");
+		
+			int quoteId = createShoppingCart();
+			testObjects.put("quoteId", quoteId);
+		}
+		catch (Exception e) {
+			e.printStackTrace();
+			fail();
+		}
+	}
+	
+	@Category({RegressionTests.class})
+	@Test
+	public void testSetShoppingCartCustomerTestCases() {
+		try {
+			MessageProcessor flow = lookupFlowConstruct("set-shopping-cart-customer");
+			MuleEvent response = flow.process(getTestEvent(testObjects));
+			
+			boolean result = (Boolean) response.getMessage().getPayload();
+			assertTrue(result);
+		}
+		catch (Exception e) {
+			e.printStackTrace();
+			fail();
+		}
+	}
+	
+	@After
+	public void tearDown() {
+		try {
+			
+		}
+		catch (Exception e) {
+			e.printStackTrace();
+			fail();
+		}
+	}
+	
+}
