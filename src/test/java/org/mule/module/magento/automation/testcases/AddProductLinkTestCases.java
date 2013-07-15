eb4db876e906e1a62fbddb67cae8b0a56b820b2a
Justin Calleja <calleja.justin@gmail.com>
2013-07-15 15:58:25 +0200

    Added AddProductLinkTestCases

---
+++ src/test/java/org/mule/module/magento/automation/testcases/AddProductLinkTestCases.java
@@ -0,0 +1,68 @@
+package org.mule.module.magento.automation.testcases;
+
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
+
+import java.util.HashMap;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.experimental.categories.Category;
+import org.mule.api.MuleEvent;
+import org.mule.api.processor.MessageProcessor;
+
+public class AddProductLinkTestCases extends MagentoTestParent {
+
+	@SuppressWarnings("unchecked")
+	@Before
+	public void setUp() {
+		try {
+			testObjects = (HashMap<String, Object>) context.getBean("addProductLink");
+
+			MessageProcessor createProductFlow = lookupFlowConstruct("create-product");
+			MuleEvent res = createProductFlow.process(getTestEvent(testObjects));
+			Integer productId = (Integer) res.getMessage().getPayload();
+			testObjects.put("productId", productId);
+			
+			// change the sku for the second product
+			testObjects.put("sku", ((String) testObjects.get("sku")) + "abc");
+			res = createProductFlow.process(getTestEvent(testObjects));
+			Integer linkedProductId = (Integer) res.getMessage().getPayload();
+			testObjects.put("linkedProductIdOrSku", linkedProductId);
+		} catch (Exception e) {
+			e.printStackTrace();
+			fail();
+		}
+	}
+
+	@Category({ SmokeTests.class, RegressionTests.class })
+	@Test
+	public void testAddProductLink() {
+		try {
+			String linkType = "related";
+			testObjects.put("type", linkType);
+			MessageProcessor addProductLinkFlow = lookupFlowConstruct("add-product-link");
+			MuleEvent res = addProductLinkFlow.process(getTestEvent(testObjects));
+			
+			assertTrue("Assert that the return type of the operation should be boolean (true)", (Boolean) res.getMessage().getPayload());
+		} catch (Exception e) {
+			e.printStackTrace();
+			fail();
+		}
+	}
+
+	@After
+	public void tearDown() {
+		try {
+			int productId = (Integer) testObjects.get("productId");
+			int linkedProductId = (Integer) testObjects.get("linkedProductIdOrSku");
+			deleteProductById(productId);
+			deleteProductById(linkedProductId);
+		} catch (Exception e) {
+			e.printStackTrace();
+			fail();
+		}
+	}
+
+}
