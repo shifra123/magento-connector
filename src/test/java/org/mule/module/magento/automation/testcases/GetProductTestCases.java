c1c98bc4dffba4fc3c94dc3fdb230369b011c195
Justin Calleja <calleja.justin@gmail.com>
2013-07-15 18:07:56 +0200

    Added GetProductTestCases.

---
+++ src/test/java/org/mule/module/magento/automation/testcases/GetProductTestCases.java
@@ -0,0 +1,63 @@
+package org.mule.module.magento.automation.testcases;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
+
+import java.util.HashMap;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.experimental.categories.Category;
+import org.mule.api.MuleEvent;
+import org.mule.api.processor.MessageProcessor;
+
+import com.magento.api.CatalogProductReturnEntity;
+
+public class GetProductTestCases extends MagentoTestParent {
+
+	@SuppressWarnings("unchecked")
+	@Before
+	public void setUp() {
+		try {
+			testObjects = (HashMap<String, Object>) context.getBean("getProduct");
+			MessageProcessor createProductFlow = lookupFlowConstruct("create-product");
+			MuleEvent response = createProductFlow.process(getTestEvent(testObjects));
+			response.getMessage().getPayload();
+			testObjects.put("productId", response.getMessage().getPayload());
+		}
+		catch (Exception e) {
+			e.printStackTrace();
+			fail();
+		}
+	}
+	
+	@Category({SmokeTests.class, RegressionTests.class})
+	@Test
+	public void testGetProduct() {
+		try {
+			MessageProcessor flow = lookupFlowConstruct("get-product");
+			
+			MuleEvent response = flow.process(getTestEvent(testObjects));
+			CatalogProductReturnEntity catalogProductReturnEntity = (CatalogProductReturnEntity) response.getMessage().getPayload();
+			
+			assertEquals("The sku of the retrieved object should be " + testObjects.get("sku"), testObjects.get("sku"), catalogProductReturnEntity.getSku());
+		}
+		catch (Exception e) {
+			e.printStackTrace();
+			fail();
+		}
+	}
+	
+	@After
+	public void tearDown() {
+		try {
+			int productId = (Integer) testObjects.get("productId");
+			deleteProductById(productId);
+		}
+		catch (Exception e) {
+			e.printStackTrace();
+			fail();
+		}
+	}
+}
