f3f5d6b1ad070c7419be51aac6e9da9b72e7f7ad
jsaliba <justin.saliba@ricston.com>
2013-07-15 13:38:09 +0200

    Added ListShoppingCartProducts test cases

--- src/test/java/org/mule/module/magento/automation/testcases/MagentoTestHelper.java
+++ src/test/java/org/mule/module/magento/automation/testcases/MagentoTestHelper.java
@@ -3,6 +3,9 @@ package org.mule.module.magento.automation.testcases;
 import java.util.ArrayList;
 import java.util.List;
 
+import com.magento.api.CatalogProductEntity;
+import com.magento.api.ShoppingCartProductEntity;
+
 public class MagentoTestHelper {
 
 	public static List<Integer> getIntegerList(List<String> stringList) {
@@ -22,6 +25,19 @@ public class MagentoTestHelper {
 		}
 		
 		return stringList;
+	}
+
+	public static boolean isProductInShoppingCart(List<CatalogProductEntity> catalogProductEntities, ShoppingCartProductEntity shoppingCartProductEntity) {
+		return isProductInShoppingCart(catalogProductEntities, shoppingCartProductEntity.getProduct_id());
+	}
+	
+	public static boolean isProductInShoppingCart(List<CatalogProductEntity> catalogProductEntities, String productId) {
+		for (CatalogProductEntity catalogProductEntity : catalogProductEntities) {
+			if (catalogProductEntity.getProduct_id().equals(productId)) {
+				return true;
+			}
+		}
+		return false;
 	}
 	
 }
