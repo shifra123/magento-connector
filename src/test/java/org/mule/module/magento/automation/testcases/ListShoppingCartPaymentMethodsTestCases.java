42a724bc6e612abb73bc3045859daa499ee8899e
jsaliba <justin.saliba@ricston.com>
2013-07-16 10:07:32 +0200

    Added SetShoppingCartPaymentMethods and ListShoppingCartPaymentMethods test cases

---
+++ src/test/java/org/mule/module/magento/automation/testcases/ListShoppingCartPaymentMethodsTestCases.java
@@ -0,0 +1,68 @@
+package org.mule.module.magento.automation.testcases;
+
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
+
+import java.util.HashMap;
+import java.util.List;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.experimental.categories.Category;
+import org.mule.api.MuleEvent;
+import org.mule.api.processor.MessageProcessor;
+
+import com.magento.api.AssociativeEntity;
+import com.magento.api.ShoppingCartCustomerAddressEntity;
+import com.magento.api.ShoppingCartPaymentMethodResponseEntityArray;
+
+public class ListShoppingCartPaymentMethodsTestCases extends MagentoTestParent {
+
+	@Before
+	public void setUp() {
+		try {
+			testObjects = new HashMap<String, Object>(); 
+
+			int quoteId = createShoppingCart();
+			testObjects.put("quoteId", quoteId);
+		}
+		catch (Exception e) {
+			e.printStackTrace();
+			fail();
+		}
+	}
+	
+	@Category({RegressionTests.class})
+	@Test
+	public void testSetShoppingCartPaymentMethodsTestCases() {
+		try {
+			MessageProcessor flow = lookupFlowConstruct("list-shopping-cart-payment-methods");
+			MuleEvent response = flow.process(getTestEvent(testObjects));
+			
+			ShoppingCartPaymentMethodResponseEntityArray cartPayments = (ShoppingCartPaymentMethodResponseEntityArray) response.getMessage().getPayload();
+			assertNotNull(cartPayments);
+			System.out.println(cartPayments.getCode());
+			System.out.println(cartPayments.getTitle());
+			for (AssociativeEntity entity : cartPayments.getCc_types()) {
+				System.out.println(entity.getKey() + " : " + entity.getValue());
+			}
+		}
+		catch (Exception e) {
+			e.printStackTrace();
+			fail();
+		}
+	}
+	
+	@After
+	public void tearDown() {
+		try {
+			
+		}
+		catch (Exception e) {
+			e.printStackTrace();
+			fail();
+		}
+	}
+}
