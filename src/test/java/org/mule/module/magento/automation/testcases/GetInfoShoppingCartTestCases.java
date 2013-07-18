04f12ddec14d92cd16a7793b810040f47908371d
jsaliba <justin.saliba@ricston.com>
2013-07-18 11:54:03 +0200

    Added get info shopping cart test cases

---
+++ src/test/java/org/mule/module/magento/automation/testcases/GetInfoShoppingCartTestCases.java
@@ -0,0 +1,58 @@
+package org.mule.module.magento.automation.testcases;
+
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.experimental.categories.Category;
+import org.mule.api.MuleEvent;
+import org.mule.api.processor.MessageProcessor;
+
+import com.magento.api.ShoppingCartCustomerEntity;
+import com.magento.api.ShoppingCartInfoEntity;
+
+public class GetInfoShoppingCartTestCases extends MagentoTestParent {
+
+	@Before
+	public void setUp() {
+		try {
+			testObjects = (Map<String, Object>) context.getBean("getInfoShoppingCart");
+			int quoteId = createShoppingCart();
+			testObjects.put("quoteId", quoteId);
+			
+			ShoppingCartCustomerEntity customer = (ShoppingCartCustomerEntity) testObjects.get("customer");
+			setShoppingCartCustomer(quoteId, customer);
+		}
+		catch (Exception e) {
+			e.printStackTrace();
+			fail();
+		}
+	}
+	
+	@Category({RegressionTests.class})
+	@Test
+	public void testGetInfoShoppingCart() {
+		try {
+			int quoteId = (Integer) testObjects.get("quoteId");
+			ShoppingCartCustomerEntity customer = (ShoppingCartCustomerEntity) testObjects.get("customer");
+			
+			MessageProcessor flow = lookupFlowConstruct("get-info-shopping-cart");
+			MuleEvent response = flow.process(getTestEvent(testObjects));
+			
+			ShoppingCartInfoEntity result = (ShoppingCartInfoEntity) response.getMessage().getPayload();
+			assertNotNull(result);
+			assertTrue(quoteId == result.getQuote_id());
+		}
+		catch (Exception e) {
+			e.printStackTrace();
+			fail();
+		}
+	}
+		
+}
