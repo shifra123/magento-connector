a4a03393e01b7a43cbb14518eb980d8825c73cd8
Justin Calleja <calleja.justin@gmail.com>
2013-07-18 17:25:28 +0200

    Added ListStockItemsTestCases

---
+++ src/test/java/org/mule/module/magento/automation/testcases/ListStockItemsTestCases.java
@@ -0,0 +1,65 @@
+package org.mule.module.magento.automation.testcases;
+
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.fail;
+
+import java.util.HashMap;
+import java.util.List;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.experimental.categories.Category;
+import org.mule.api.MuleEvent;
+import org.mule.api.processor.MessageProcessor;
+
+import com.magento.api.CatalogInventoryStockItemEntity;
+
+public class ListStockItemsTestCases extends MagentoTestParent {
+
+	@SuppressWarnings("unchecked")
+	@Before
+	public void setUp() {
+		try {
+			testObjects = (HashMap<String, Object>) context.getBean("listStockItems");
+			MessageProcessor createProductFlow = lookupFlowConstruct("create-product");
+			MuleEvent response = createProductFlow.process(getTestEvent(testObjects));
+			response.getMessage().getPayload();
+			testObjects.put("productId", response.getMessage().getPayload());
+		}
+		catch (Exception e) {
+			e.printStackTrace();
+			fail();
+		}
+	}
+	
+	@SuppressWarnings("unchecked")
+	@Category({SmokeTests.class, RegressionTests.class})
+	@Test
+	public void testListStockItems() {
+		try {
+			MessageProcessor flow = lookupFlowConstruct("list-stock-items");
+			
+			MuleEvent response = flow.process(getTestEvent(testObjects));
+			List<CatalogInventoryStockItemEntity> result = (List<CatalogInventoryStockItemEntity>) response.getMessage().getPayload();
+			assertNotNull(result);
+		}
+		catch (Exception e) {
+			e.printStackTrace();
+			fail();
+		}
+	}
+	
+	@After
+	public void tearDown() {
+		try {
+			int productId = (Integer) testObjects.get("productId");
+			deleteProductById(productId);
+		}
+		catch (Exception e) {
+			e.printStackTrace();
+			fail();
+		}
+	}
+	
+}
