9edc03b97f7eb3f2ba2bd9d95b16a80ccc4c5740
Justin Calleja <calleja.justin@gmail.com>
2013-07-18 18:00:18 +0200

    Added UpdateInventoryStockItemTestCases.

---
+++ src/test/java/org/mule/module/magento/automation/testcases/UpdateInventoryStockItemTestCases.java
@@ -0,0 +1,61 @@
+package org.mule.module.magento.automation.testcases;
+
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.fail;
+
+import java.util.HashMap;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.experimental.categories.Category;
+import org.mule.api.MuleEvent;
+import org.mule.api.processor.MessageProcessor;
+
+public class UpdateInventoryStockItemTestCases extends MagentoTestParent {
+
+	@SuppressWarnings("unchecked")
+	@Before
+	public void setUp() {
+		try {
+			testObjects = (HashMap<String, Object>) context.getBean("updateInventoryStockItem");
+			MessageProcessor flow = lookupFlowConstruct("create-product");
+			MuleEvent res = flow.process(getTestEvent(testObjects));
+			Integer productId = (Integer) res.getMessage().getPayload();
+			testObjects.put("productId", productId);
+		}
+		catch (Exception e) {
+			e.printStackTrace();
+			fail();
+		}
+	}
+	
+	@Category({SmokeTests.class, RegressionTests.class})
+	@Test
+	public void testUpdateInventoryStockItem() {
+		try {
+			MessageProcessor flow = lookupFlowConstruct("update-inventory-stock-item");
+			MuleEvent response = flow.process(getTestEvent(testObjects));
+			
+			Integer result = (Integer) response.getMessage().getPayload();
+			assertNotNull(result);
+		}
+		catch (Exception e) {
+			e.printStackTrace();
+			fail();
+		}
+	}
+	
+	@After
+	public void tearDown() {
+		try {
+			int productId = (Integer) testObjects.get("productId");
+			deleteProductById(productId);
+		}
+		catch (Exception e) {
+			e.printStackTrace();
+			fail();
+		}
+	}
+	
+}
