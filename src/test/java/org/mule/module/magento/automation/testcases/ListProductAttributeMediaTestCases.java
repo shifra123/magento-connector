d96432f03ba8ab9b517d00fad192fbeed8f4caba
Justin Calleja <calleja.justin@gmail.com>
2013-07-19 16:22:44 +0200

    Added ListProductAttributeMediaTestCases.

---
+++ src/test/java/org/mule/module/magento/automation/testcases/ListProductAttributeMediaTestCases.java
@@ -0,0 +1,64 @@
+package org.mule.module.magento.automation.testcases;
+
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.fail;
+
+import java.util.HashMap;
+import java.util.List;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.experimental.categories.Category;
+import org.mule.api.MuleEvent;
+import org.mule.api.processor.MessageProcessor;
+
+import com.magento.api.CatalogProductImageEntity;
+
+public class ListProductAttributeMediaTestCases extends MagentoTestParent {
+
+	@SuppressWarnings("unchecked")
+	@Before
+	public void setUp() {
+		try {
+			testObjects = (HashMap<String, Object>) context.getBean("listProductAttributeMedia");
+
+			MessageProcessor createProductFlow = lookupFlowConstruct("create-product");
+			MuleEvent res = createProductFlow.process(getTestEvent(testObjects));
+			Integer productId = (Integer) res.getMessage().getPayload();
+			testObjects.put("productId", productId);
+		} catch (Exception e) {
+			e.printStackTrace();
+			fail();
+		}
+	}
+
+	@SuppressWarnings("unchecked")
+	@Category({ SmokeTests.class, RegressionTests.class })
+	@Test
+	public void testListProductAttributeMedia() {
+		try {
+			MessageProcessor listProductLinkFlow = lookupFlowConstruct("list-product-attribute-media");
+			MuleEvent res = listProductLinkFlow.process(getTestEvent(testObjects));
+			List<CatalogProductImageEntity> catalogProductImageEntities = (List<CatalogProductImageEntity>) res.getMessage().getPayload();
+			
+			assertNotNull(catalogProductImageEntities);
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+			fail();
+		}
+	}
+
+	@After
+	public void tearDown() {
+		try {
+			int productId = (Integer) testObjects.get("productId");
+			deleteProductById(productId);
+		} catch (Exception e) {
+			e.printStackTrace();
+			fail();
+		}
+	}
+
+}
